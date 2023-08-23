9e540e088b6b2cc01347188827833692802fa312
Raman-Singh-Shekhawat <rs8893@srmist.edu.in>
2023-08-23 16:42:04 +0530

    Code further enhanced

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import * as React from 'react'
 import './App.css';
-import ReactDOM from 'react-dom'
 import Editor from './components/Editor';
 import LivePreview from './components/LivePreview';
-import ToolTip from './components/ToolTip';
 import './Styles/Editor.css';
 import './Styles/LivePreview.css'
 import './Styles/ToolTip.css'
-import { currentElement } from './components/Editor';
 
 function App() {
 
